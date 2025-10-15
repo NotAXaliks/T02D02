f6a15ee11dea747f9f380e4dfbd729e99b0016bc
Xaliks <me@xaliks.dev>
2025-10-15 19:26:21 +0300

    Added Created At column to Orders

--- src/CampusBenchApi/CampusBenchApi/Models/Order.cs
+++ src/CampusBenchApi/CampusBenchApi/Models/Order.cs
@@ -5,6 +5,7 @@ public partial class Order
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
     
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
