d7057a66c2b33fd24d9072ece5056318d1b46fca
Xaliks <me@xaliks.dev>
2025-10-15 19:04:30 +0300

    Init В снапшоте по какой то причине был созданный DateTime, однако же в модели его нет, из за чего выдавало ошибку при синхронизации

--- src/CampusBenchApi/CampusBenchApi/Controllers/OrderController.cs
+++ src/CampusBenchApi/CampusBenchApi/Controllers/OrderController.cs
@@ -44,19 +44,19 @@ namespace CampusBenchApi.Controllers
 
         // PUT: api/Order/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutOrder(int id, Order order)
-        {
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutOrder(int id, Order order)
+        // {
            
-        }
+        // }
 
         // POST: api/Order
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Order>> PostOrder(Order order)
-        {
+        // [HttpPost]
+        // public async Task<ActionResult<Order>> PostOrder(Order order)
+        // {
            
-        }
+        // }
 
         // DELETE: api/Order/5
         [HttpDelete("{id}")]
